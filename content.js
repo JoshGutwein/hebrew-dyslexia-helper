115dd18671b133f0197a3471045584cd5d201614
Josh <joshgutwein816@gmail.com>
2026-03-22 11:17:44 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,1 +1,13 @@
+document.getElementById("applyBtn").addEventListener("click", async () => {
+  const spacingOn = document.getElementById("spacingToggle").checked;
 
+  const [tab] = await chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  });
+
+  chrome.tabs.sendMessage(tab.id, {
+    action: "applySpacing",
+    spacingOn: spacingOn
+  });
+});
